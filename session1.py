7c3acd6b28ba1155782adce6129c04532c1e4c57
Mahdi Nasirsoltani <mnasirsoltani@gmail.com>
2025-07-26 01:36:47 +0330

    Add session1 code snippets

---
+++ session1.py
@@ -0,0 +1,51 @@
+# Our first code showing Python's simple syntax
+print("Hello World")
+print('Hello World')
+
+# Using print() with 4 types of pythonic data
+# string (str)
+print("Hey")
+
+# integer (int)
+print(2)
+
+# float
+print(4.3)
+
+# boolean (bool)
+print(True)
+print(False)
+
+# variables
+name = "Dimitri"
+print(name)
+
+num = 5
+print(num)
+
+number = 4.6
+print(number)
+
+happy = True
+print(happy)
+
+# rules 3 + 1
+# Can only use alpha, nums and underscores(_) while creating variables
+car = "Mercedes"
+car2 = "BMW"
+car_3 = "Lincoln Continental Mark III 1976"
+print(car)
+print(car2)
+print(car_3)
+
+# Can't start with numbers
+# 1car = 'Peykan'
+
+# Variables are case sensitive
+Club = 'Ajax'
+club = 'Barcelona'
+print(club)
+print(Club)
+
+# No Pythonic words
+# break = "Karim"
