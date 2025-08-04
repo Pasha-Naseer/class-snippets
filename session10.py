fdd0e5dbccd34cdcfb1dbc6c1669e3aeadf3c471
Mahdi Nasirsoltani <mnasirsoltani@gmail.com>
2025-08-04 22:49:14 +0330

    Add session10.py

---
+++ session10.py
@@ -0,0 +1,35 @@
+import math
+
+# NameError
+# print(a)
+
+# TypeError
+# print(1 + "1")
+
+# Value Error
+# print(math.sqrt(-9))
+#  a = float("Error")
+
+# IndexError
+# a = [1, 2, 3]
+# print(a[9])
+
+# IndentationError
+# for i in range(1, 3):
+# print(i)
+
+# ModuleNotFoundError
+# import physics
+
+# ZeroDivisionError
+# print(5 / 0)
+
+# SyntaxError
+# print("Hey)
+
+# KeyError
+# my_dict = {"Key": "value"}
+# print(my_dict["lock"])
+
+# OverflowError
+# print(math.exp(2000))
