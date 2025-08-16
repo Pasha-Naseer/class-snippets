eff19395a5b313a094d1b9125dcd0bea05ae1645
Mahdi Nasirsoltani <mnasirsoltani@gmail.com>
2025-08-17 01:23:21 +0330

    Add session17.py

---
+++ session17.py
@@ -0,0 +1,59 @@
+# operators
+# arithmetic operators
+print(1 + 2)
+print(2 - 1)
+print(2 * 3)
+print(4 // 2)
+print(5 / 2)
+print(3 ** 2)
+print(5 % 2)
+
+# comparison operators
+print(2 > 3)
+print(2 < 3)
+print(2 >= 2)
+print(2 <= 4)
+print(2 == 3)
+print(2 != 3)
+
+# logical operators
+print(1 and 2 < 3)
+print(1 or 4 < 3)
+print(str is not str)
+print(str is not int)
+
+# bitwise operators
+print('and')
+print(0 & 1)
+print(1 & 0)
+print(0 & 0)
+print(1 & 1)
+print('*' * 20)
+print('or')
+print(0 | 0)
+print(0 | 1)
+print(1 | 0)
+print(1 | 1)
+print('*' * 20)
+print('xor')
+print(0 ^ 0)
+print(0 ^ 1)
+print(1 ^ 0)
+print(1 ^ 1)
+print('*' * 20)
+print('not')
+zero = 13
+print(~zero)
+print(40 >> 2)
+print(40 << 2)
+
+# assignment operators
+assignment = 1
+
+# identity operators
+print(str is str)
+print(str is not str)
+
+# membership operators
+print('item1' in ['item1', 'item2', 'item3'])
+print('item10' not in ['item1', 'item2', 'item3'])
