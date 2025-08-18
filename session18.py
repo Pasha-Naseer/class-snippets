f2955d2cc9f25fe9a972e54104fd96639864b063
Mahdi Nasirsoltani <mnasirsoltani@gmail.com>
2025-08-19 01:07:15 +0330

    Add session18.py

---
+++ session18.py
@@ -0,0 +1,12 @@
+# escape sequence
+print('hello\rworld')
+print('hello\nworld')
+print('hello\tworld')
+print('hello\\world')
+print('hello\'world')
+print('hello,\bworld')
+print('hello\fworld')
+print('\102')  # ascii
+print("\110\105\114\114\117\040\127\117\122\114\104")  # ascii
+print('\x40')  # ascii
+print("\x48\x45\x4C\x4C\x4F\x20\x57\x4F\x52\x4C\x44")  # ascii
