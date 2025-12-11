f9f2e277c3450704222644677a74c4c77d3968ef
Mahdi Nasirsoltani <mnasirsoltani@gmail.com>
2025-12-11 23:51:06 +0330

    debug sesssion9

--- session9.py
+++ session9.py
@@ -16,7 +16,7 @@ def p_prime(number):
 
 def prime(number):
     if number > 1:
-        for i in range(2, number):
+        for i in range(2, math.ceil(math.sqrt(number))):
             if number % i == 0:
                 return None
         else:
