cccab9533601bb685d4a8abda4422b084e63363b
Mahdi Nasirsoltani <mnasirsoltani@gmail.com>
2026-01-03 13:53:01 +0330

    Fix calculation of smaller and bigger angle, wrap code inside function for reusing my code with the currnet local time, add loops and try-except blocks for input control, add strftime.

--- angle_between_clock_hands.py
+++ angle_between_clock_hands.py
@@ -1,22 +1,72 @@
-angle_between_consecutive_hour_marks = 360 / 12
-angle_between_consecutive_minute_marks = 360 / 60
-additional_hour_angle_per_every_minute = 30 / 60
-hour = int(input("Enter hour: "))
-minute = int(input("Enter minute: "))
-hour_angle = hour * angle_between_consecutive_hour_marks + (minute * additional_hour_angle_per_every_minute)
-minute_angle = minute * angle_between_consecutive_minute_marks
+import time
+import datetime
 
-if hour_angle > minute_angle:
-    angle = hour_angle - minute_angle
-elif hour_angle < minute_angle:
-    angle = minute_angle - hour_angle
-else:
-    angle = 0
+def angle_between_clock_hands(_hour, _minute):
+    angle_between_consecutive_hour_marks = 360 / 12  # 30
+    angle_between_consecutive_minute_marks = 360 / 60  # 6
+    additional_hour_angle_per_every_minute = 30 / 60  # 0.5
+    hour = _hour
+    minute = _minute
+    hour_angle = hour * angle_between_consecutive_hour_marks + (minute * additional_hour_angle_per_every_minute)
+    minute_angle = minute * angle_between_consecutive_minute_marks
 
-angle2 = 360 - angle
-print(f"smaller: {angle}")
-print(f"Bigger angle: {angle2}")
-# cannot identify smaller and bigger angel sometimes
+    # print(f"hour angle: {hour_angle}")
+    # print(f"minute angle: {minute_angle}")
+
+    if hour_angle > minute_angle:
+        angle = hour_angle - minute_angle
+    elif hour_angle < minute_angle:
+        angle = minute_angle - hour_angle
+    else:
+        angle = 0
+
+    if angle > 180:
+        angle2 = 360 - angle
+        print(f"smaller angle: {angle2}")
+        print(f"Bigger angle: {angle}")
+    elif angle < 180:
+        angle2 = 360 - angle
+        print(f"smaller angle: {angle}")
+        print(f"Bigger angle: {angle2}")
+    else:
+        print("big angle and small angle are equal!")
+        print(angle)
+
+# input custom hour
+while True:
+    try:
+        h = int(input("Enter hour: "))
+    except ValueError:
+        print("Invalid Input!")
+    else:
+        if h in range(0, 13):
+            break
+        elif h in range(13, 25):
+            h -= 12
+            break
+        else:
+            print("Invalid Input!")
+
+# input custom minute
+while True:
+    try:
+        m = int(input("Enter minute: "))
+    except ValueError:
+        print("Invalid Input!")
+    else:
+        if m in range(0, 60):
+            break
+        else:
+            print("Invalid Input!")
+
+angle_between_clock_hands(h, m)
+print("*"*30)
+# current angle between the time hands
+print("The angle between the current time clock hands:")
+print(datetime.datetime.now().strftime("%I : %M %p"))
+h = int(datetime.datetime.now().strftime("%#I"))
+m = int(datetime.datetime.now().minute)
+angle_between_clock_hands(h, m)
+
 # I will add seconds to be able to track the exact overlap where the angle is 0!
-# This ccode will be updated
-# Bugs are yet to be fixed!
+# This code will be updated
